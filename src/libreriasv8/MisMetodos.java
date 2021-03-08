feacaebc44b2c0cfc33577f95b8ad7315034fed6
hfaildeestevez <72596856+hfaildeestevez@users.noreply.github.com>
2021-03-08 21:12:45 +0100

    Add files via upload

---
+++ src/libreriasv8/MisMetodos.java
@@ -0,0 +1,15 @@
+package libreriasv8;
+
+/**
+ * Contiene metodos utiles para usar en los proyectos
+ */
+public class MisMetodos {
+    /**
+     * Metodo que monta un saludo
+     * @param mensaje
+     * @return
+     */
+    public static String saludo(String mensaje) {
+        return "Saldudo Modificacion " + mensaje;
+    }
+}
